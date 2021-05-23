89cac58029fe2d9668602c88e5d00cf3933bed7c
ayxan261 <75589081+ayxan261@users.noreply.github.com>
2021-05-23 14:31:19 +0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EdliyyeProduct.Models.Entity;
+
+namespace EdliyyeProduct.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        EdliyyeEntityEntities db = new EdliyyeEntityEntities();
+        public ActionResult Index(string ara)
+        {
+            var model = db.Possts.ToList();
+            if (!string.IsNullOrEmpty(ara))
+            {
+                model = db.Possts.Where(x => x.Title.Contains(ara)).ToList();
+            }
+            return View(model);
+        }
+
+        public ActionResult CommentPage(int id)
+        {
+            var model = db.Possts.Find(id);
+            var cmp = db.Comments.Where(x => x.PostId == model.Id).ToList();
+            return View(cmp);
+        }
+    }
+}
